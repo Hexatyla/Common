dff13346461a718ce3648f2330aa7b5de77be8fa
Geoffrey Bauduin <bauduin.geo@gmail.com>
2014-03-30 14:48:10 +0200

    removed userjob from factory/network

--- includes/Factory/Network.hpp
+++ includes/Factory/Network.hpp
@@ -5,25 +5,18 @@
 // Login   <lefebv_n@epitech.net>
 // 
 // Started on  Fri Jan 24 15:25:56 2014 gautier lefebvre
-// Last update Thu Feb 20 17:46:02 2014 gautier lefebvre
+// Last update Sun Mar 30 14:47:24 2014 geoffrey bauduin
 //
 
 #ifndef		__FACTORY_NETWORK_HPP__
 #define		__FACTORY_NETWORK_HPP__
 
-#include	"Network/UserJob.hpp"
 #include	"Network/Socket.hpp"
 
 namespace	Factory {
   namespace	Network {
     void	init();
     void	end();
-
-    class	UserJob {
-    public:
-      static ::Network::UserJob	*create(::Network::SSocket *s = NULL, ::Protocol::Job *j = NULL);
-      static void		remove(::Network::UserJob*);
-    };
 
     class	SSocket {
     public:
